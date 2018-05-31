85e7a7ff1021a5c72cb664fbf73feb76815a9fd5
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2018-05-31 18:03:49 -0400

    Primality test
    Primality test

---
+++ 01_Primesum.java
@@ -0,0 +1,40 @@
+/** @author D/L */
+/** Q: What is the sum of the first 123456 prime numbers? */
+/** A: use simple primality test */
+
+public class Primesum {
+
+    public static void main(String[] args) {
+        
+        int count = 0;
+        int number = 2;
+        long sum = 0;
+        
+        while(count < 123456){
+            if(isPrimeNumber(number)){
+                sum += number;
+                count++;
+            }
+            number++;
+        }
+        System.out.println(sum);
+    }
+
+    private static boolean isPrimeNumber(int number){
+        
+        if (number <= 1) return false;
+        
+        if (number <= 3) return true;
+ 
+        if (number % 2 == 0 || number % 3 == 0) return false;
+ 
+        for ( int i = 5; i*i <= number; i = i + 6) {
+            if ( number % i == 0 || number % (i+2) == 0)
+                return false;
+        }
+ 
+        return true;
+    }
+}
+
+    
