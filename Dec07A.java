976a96d2b9f8c23eab7e037301bf23b1cc1b5f55
sylabtechnologies <teslacloud@icloud.com>
2020-12-08 16:22:17 -0500

    fix

--- Dec07A.java
+++ Dec07A.java
@@ -9,14 +9,17 @@ import java.util.*;
 
 class Triangle
 {
+    private int mySize = 5;
     char arr[][];
     
-    public Triangle()
+    public Triangle(int sz)
     {
-        arr = new char[5][];
-        for (int i = 0; i < 5; i++)
+        mySize = sz;
+        
+        arr = new char[mySize][];
+        for (int i = 0; i < mySize; i++)
         {
-            arr[i] = new char[5];
+            arr[i] = new char[mySize];
             Arrays.fill(arr[i], ' ');
             
             for (int j = 0; j < i+1; j++)
@@ -30,8 +33,8 @@ class Triangle
     {
         for (char[] row : arr)
         {
-            int i = 0, j = row.length - 1;
-            while (i != j)
+            int i = 0, j = mySize - 1;
+            while (i <= j)
             {
                 char tmp = row[i];
                 row[i] = row[j];
@@ -44,7 +47,7 @@ class Triangle
     void flipHoriz()
     {
         int i = 0, j = arr.length - 1;
-        while (i != j)
+        while (i <= j)
         {
             char tmpRow[] = arr[i];
             arr[i] = arr[j];
@@ -52,6 +55,11 @@ class Triangle
             i++; j--;
         }
     }
+
+    public int getSize() {
+        return mySize;
+    }
+    
 }
 
 public class Dec07A
@@ -60,20 +68,20 @@ public class Dec07A
 
     public static void main(String[] args)
     {
-        Triangle tr = new Triangle();
+        Triangle tr = new Triangle(4);
         
         prepare(1, tr, 0, 0);
 
         tr.flipVert();
         prepare(2, tr, 0, 0);
         
-        Triangle tr2 = new Triangle();
-        prepare(3, tr, 0, 0);
+        Triangle tr2 = new Triangle(4);
+        prepare(3, tr, 0, 1);
         prepare(3, tr2, 0, 4);
 
         tr.flipHoriz();
         tr2.flipHoriz();
-        prepare(4, tr, 0, 0);
+        prepare(4, tr, 0, 1);
         prepare(4, tr2, 0, 4);
 
         for (ArrayList<String> lst : mybuf)
@@ -86,7 +94,7 @@ public class Dec07A
     }
 
     // prepping all parts
-    private static void prepare(int row, Triangle tr, int y, int x)
+    private static void prepare(int row, Triangle tr, int ystart, int xstart)
     {
         // prep add
         if (row - 1 == mybuf.size())
@@ -95,17 +103,17 @@ public class Dec07A
             
             cur.add(row + ")");
             
-            for (int i = 1; i < y; i++)
+            for (int i = 1; i < ystart; i++)
             {
                 cur.add("");                
             }
             
             String filr = null;
-            if (x > 0)
+            if (xstart > 0)
             {
-                char temp[] = new char[x];
+                char temp[] = new char[xstart];
                 Arrays.fill(temp, ' ');
-                filr = new String(filr);
+                filr = new String(temp);
             }
             
             for (char[] trRow : tr.arr)
@@ -114,7 +122,7 @@ public class Dec07A
                 cur.add(prn);
             }
 
-            char temp[] = new char[10];
+            char temp[] = new char[tr.getSize() * 2 + 1];
             Arrays.fill(temp, '-');
             cur.add(new String(temp));
             
@@ -123,7 +131,7 @@ public class Dec07A
         // prep replace
         else if (row == mybuf.size())
         {
-            for (int i = 1; i < 6; i++)
+            for (int i = 1; i < tr.getSize() + 1; i++)
             {
                 String cur = mybuf.get(row-1).get(i);
                 ArrayList<Character> temp = new ArrayList<>();
@@ -131,11 +139,11 @@ public class Dec07A
                     temp.add(c);
                 
                 char[] trRow = tr.arr[i-1];
-                for (int j = temp.size(); j < x + trRow.length; j++)
+                for (int j = temp.size(); j < xstart + trRow.length; j++)
                     temp.add(' ');
                 
-                for (int j = x; j < x + trRow.length; j++)
-                    temp.set(j, trRow[j-x]);
+                for (int j = xstart; j < xstart + trRow.length; j++)
+                    temp.set(j, trRow[j-xstart]);
  
                 StringBuilder sb = new StringBuilder();
                 for (Character ch : temp) sb.append(ch);
--- Dec07B.java
+++ Dec07B.java
@@ -10,9 +10,11 @@ import java.util.Random;
 
 public class Dec07B
 {
+    private final static int MAXNUM = 100;
+    
     public static void info()
     {
-        System.out.println("Guess the number between 1 and 1000:");
+        System.out.println("Guess the number between 1 and " + MAXNUM + ":");
         System.out.println("");
     }    
     
@@ -21,29 +23,35 @@ public class Dec07B
         info();
         
         Random rand = new Random();
-        int mustguess = rand.nextInt(1000) + 1;
+        int mustguess = rand.nextInt(MAXNUM) + 1;
 
         Scanner input = new Scanner(System.in);
-        int guess = 0;
+        int guess = 0, cnt = 0;
+        boolean found = false;
 
-        while(true)
+        while(!found && cnt < 5)
         {
             System.out.print("Your guess? ");
             guess = input.nextInt();
             
-            if (Math.abs(guess - mustguess) <= 100)
-                break;
-            else if(guess > mustguess)
+            if (Math.abs(guess - mustguess) <= 10)
             {
+                found = true;
+                continue;
+            }
+            
+            cnt++;
+            if(guess > mustguess)
                 System.out.println("It\'s lower.");
-            }
             else
-            {
                 System.out.println("It\'s higher.");
-            }
         }
 
-        System.out.println("Nice job!");
-        System.out.println("Your number is: " + mustguess);
+        if (found)
+            System.out.print("Nice job! ");
+        else
+            System.out.println("Sorry!");
+            
+        System.out.print("Your number is: " + mustguess);
     }
 }
