bc0510b5c789fde2a7f68429e9455097dbce3f90
sylabtechnologies <teslacloud@icloud.com>
2020-12-20 18:34:48 -0500

    ini

--- Utopia/BookingController.java
+++ Utopia/BookingController.java
@@ -1,3 +1,5 @@
+package com.utopia.booking.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -9,40 +11,82 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.smoothstack.dec2020.model.Booking;
-import com.smoothstack.dec2020.service.BookingService;
+import com.utopia.booking.model.Booking;
+import com.utopia.booking.service.BookingService;
 
 @RestController
 public class BookingController {
 
 	@Autowired
-	BookingService bookingService;
+	BookingService service;
 
-	@Value("${flightcapacity.url}")
-	String flightUrl;
+	@Value("${flight.port}")
+	String flightPort;
+
+	@Value("${flight.decrement.url}")
+	String decrement;
+
+	@Value("${flight.increment.url}")
+	String increment;
+
+	private static final String host = "127.0.0.1:";
+
+	@RequestMapping(path = "booking/{bookingId}", method = RequestMethod.GET)
+	public Booking get(@PathVariable int bookingId)
+	{
+		return service.getBooking(bookingId);
+	}
 
 	@RequestMapping(path = "booking/{flightId}/{bookerId}", method = RequestMethod.POST)
-	public Booking createBooking(@PathVariable int flightId, @PathVariable int bookerId)
+	public Booking create(@PathVariable int flightId, @PathVariable int bookerId)
 	{
 		RestTemplate req = new RestTemplate();
+		ResponseEntity<String> resp =
+			req.getForEntity(host + flightPort + "/" + decrement + "/" + flightId, String.class);
 
-		ResponseEntity<String> response =
-			req.getForEntity(flightUrl + flightId, String.class);
+		if (resp.getStatusCode() != HttpStatus.OK)
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "flight error");
 
-		if (response.getStatusCode() != HttpStatus.OK)
-			throw new ResponseStatusException(response.getStatusCode(), "bad request");
+		if (!resp.getBody().equals("OK"))
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "unavailable");
 
-		if (response.getBody().equals("{}"))
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "not available");
+		return service.create(flightId, bookerId);
+	}
 
-		return bookingService.createBooking(flightId, bookerId);
+	// deactivate booking and increase flight capacity
+	@RequestMapping(path = "booking/pruned/{bookingId}", method = RequestMethod.PUT)
+	public void rollback(@PathVariable int bookingId)
+	{
+		Booking book = service.getBooking(bookingId);
+
+		if (book == null)
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "cant find");
+
+		book.setIsActive(0);
+		service.update(book);
+
+		RestTemplate req = new RestTemplate();
+		ResponseEntity<String> resp =
+			req.getForEntity(host + flightPort + "/" + increment + "/" + book.getFlightId(), String.class);
+
+		if (resp.getStatusCode() != HttpStatus.OK)
+			throw new ResponseStatusException(HttpStatus.FAILED_DEPENDENCY, "flight error");
+
+		if (!resp.getBody().equals("OK"))
+			throw new ResponseStatusException(HttpStatus.FAILED_DEPENDENCY, "flight error");
 	}
 
 	@RequestMapping(path = "booking/inactive/{bookingId}", method = RequestMethod.PUT)
-	public void deactivateBooking(@PathVariable int bookingId)
+	public void deactivate(@PathVariable int bookingId)
 	{
-		if (!bookingService.deactivateBooking(bookingId))
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "bad request");
+		Booking book = service.getBooking(bookingId);
+
+		if (book == null)
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "cant find");
+
+		book.setIsActive(0);
+		service.update(book);
 	}
 
 }
+
